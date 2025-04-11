9c53d1f2e830b4138bf1b042d0dca8bbe1282ef4
Odin Wermeille <odin.wermeille@heig-vd.ch>
2025-04-11 15:18:16 +0200

    Modified mosaic to expect a blob

--- mosaic/src/views/MosaicView.vue
+++ mosaic/src/views/MosaicView.vue
@@ -10,21 +10,43 @@ const imageWidth = ref(100);
 
 const socket = io("ws://localhost:4000"); //change to storage API url on deployment
 
+function convertToBase64(data) {
+    // Create a string from the data array without exceeding the call stack size
+    let binaryString = "";
+    for (let i = 0; i < data.length; i++) {
+        binaryString += String.fromCharCode(data[i]);
+    }
+
+    // Return the Base64 encoded string
+    return btoa(binaryString);
+}
+
+// Fetch images at startup
 const fetchImages = async () => {
   try {
     const res = await fetch("/api/images");
     const data = await res.json();
-    photos.value = data;
+
+    // Convert all images to Base64 before storing in photos
+    photos.value = data.map(photo => ({
+      ...photo,
+      img: convertToBase64(photo.img.data)
+    }));
+
   } catch (error) {
     console.error("Error fetching images:", error);
   }
 };
 
 onMounted(() => {
-  fetchImages();      
+  fetchImages();  
+
+  // Listen for new images from the server
   socket.on("newImage", (newImage) => {
+    console.log(convertToBase64(newImage.img.data));
+    newImage.img = convertToBase64(newImage.img.data);
     photos.value.push(newImage);
-    console.log(photos.value.length);
+    console.log("Updated photos:", photos.value);
   });
 });
 
@@ -41,8 +63,8 @@ onBeforeUnmount(() => {
     <img class="background" src="https://servettefc.ch/wp-content/uploads/2025/03/Article-Visuel-SFCCF-2-630x320.jpg" :style="`width: ${imageWidth}vw; height: ${imageHeight}vw`"></img>
   </div>
   <div class="pixel" v-for="n in (imageHeight/pixelHeight)*(imageWidth/pixelWidth)">
-    <img class="user-img" :id="`user-img-${n}`" src="https://thispersondoesnotexist.com/" :style="`top: ${((n-1)*pixelHeight)%imageHeight}vw; left:  ${Math.floor((n-1)/(imageHeight/pixelHeight))*pixelWidth}vw; opacity: 0.3; width: ${pixelWidth}vw; height: ${pixelHeight}vw`"></img>
-    <img class="image-blocker" :id="`image-blocker-${n}`" src="https://images.unsplash.com/photo-1598812866501-87ad598839e7?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZvbmQlMjBub2lyfGVufDB8fDB8fHww" :style="`top: ${((n-1)*pixelHeight)%imageHeight}vw; left:  ${Math.floor((n-1)/(imageHeight/pixelHeight))*pixelWidth}vw; opacity: ${Math.floor(Math.random()*2)}; width: ${pixelWidth+0.002}vw; height: ${pixelHeight+0.002}vw`"></img>
+    <img class="user-img" :id="`user-img-${n}`" :src="`data:image/jpeg;base64,${photos[n-1]?.img}`" :style="`top: ${((n-1)*pixelHeight)%imageHeight}vw; left:  ${Math.floor((n-1)/(imageHeight/pixelHeight))*pixelWidth}vw; opacity: 0.3; width: ${pixelWidth}vw; height: ${pixelHeight}vw`"></img>
+    <img class="image-blocker" :id="`image-blocker-${n}`" src="https://images.unsplash.com/photo-1598812866501-87ad598839e7?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZvbmQlMjBub2lyfGVufDB8fDB8fHww" :style="`top: ${((n-1)*pixelHeight)%imageHeight}vw; left:  ${Math.floor((n-1)/(imageHeight/pixelHeight))*pixelWidth}vw; opacity: ${photos[n-1] ? 0 : 1}; width: ${pixelWidth+0.002}vw; height: ${pixelHeight+0.002}vw`"></img>
   </div>
 </template>
 
